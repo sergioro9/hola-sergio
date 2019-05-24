81a1dd9d9fcc5d7770fe6fc25c6cc83c7f6622c4
sergioro <25378688+sergioro9@users.noreply.github.com>
2019-05-23 23:17:35 -0600

    tests passed

--- test/test_hola.rb
+++ test/test_hola.rb
@@ -1,17 +1,16 @@
 require 'minitest/autorun'
-require 'hola'
+require 'hola-sergio'
 
-class HolaTesst < Minitest::Test
-    def test_english_hello
-        assert_equal 'hello world',
-            Hola.hi('english')
-    end
-    def test_any_hello
-        assert_equal 'hello world',
-            Hola.hi('ruby')
-    end
-    def test_spanish_hello
-        assert_equal 'hola mundo',
-            Hola.hi('spanish')
-    end
+class HolaTest < Minitest::Test
+  def test_english_hello
+    assert_equal 'hello world', Hola.hi('english')
+  end
+
+  def test_any_hello
+    assert_equal 'hello world', Hola.hi('ruby')
+  end
+
+  def test_spanish_hello
+    assert_equal 'hola mundo', Hola.hi('spanish')
+  end
 end
