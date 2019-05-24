9c341ff6c8f530af0571b3ace3a60257762cb17d
sergioro <25378688+sergioro9@users.noreply.github.com>
2019-05-23 20:13:27 -0600

    created module Hola

--- lib/hola-sergio.rb
+++ lib/hola-sergio.rb
@@ -1,8 +1,11 @@
-class Hola
-    def self.hi(language="english")
-        translator=Translator.new(language)
-        translator.hi
-    end
-end
+require "hola-sergio/version"
+require "hola-sergio/translator"
 
-require 'hola-sergio/translator'
+module Hola
+  class Hola
+    def self.hi(language="english")
+      translator=Translator.new(language)
+      translator.hi
+    end
+  end
+end
